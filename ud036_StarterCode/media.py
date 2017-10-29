c10750fd9e9a2de5250b70012472a0ab440e8203
Beau Kinstler <git@beaukinstler.info>
2017-10-29 15:26:37 -0400

    Add the Udacity FSWD project 1 to the repo

---
+++ ud036_StarterCode/media.py
@@ -0,0 +1,21 @@
+'''
+Movie Class
+- Attributes
+- trailer_youtube_url
+- title
+- poster_image_url
+- 
+
+'''
+class Movie():
+
+    def __init__(self, title, description, poster_image_url, trailer_youtube_url):
+        
+        self.title = title
+        self.description = description
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+
+
+
+
